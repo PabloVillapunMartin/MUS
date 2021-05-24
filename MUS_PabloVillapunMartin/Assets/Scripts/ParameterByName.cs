e6667487504fa1670800382154a5a28b4bb496ba
PabloVillapunMartin <48910465+PabloVillapunMartin@users.noreply.github.com>
2021-05-24 13:57:40 +0200

    Modificacion de parametros FMOD desde Unity

---
+++ MUS_PabloVillapunMartin/Assets/Scripts/ParameterByName.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParameterByName : MonoBehaviour
+{
+    public string parameterName = "";
+
+    FMOD.Studio.EventInstance prueba;
+    void Start()
+    {
+        prueba = FMODUnity.RuntimeManager.CreateInstance("event:/Prueba");
+        prueba.start();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            prueba.setParameterByName(parameterName, 1f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            prueba.setParameterByName(parameterName, 0f);
+        }
+    }
+}
